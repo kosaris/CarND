4747d5d5b3c880a0e9faf0d8d5c998fafce62d89
sina <kosari@uw.edu>
2017-12-31 18:44:01 -0800

    perform autoformat with yapf to conform with google style

--- TensorFlow-Sandbox/example1/example1_and_gate_with_logistic_regression.py
+++ TensorFlow-Sandbox/example1/example1_and_gate_with_logistic_regression.py
@@ -2,41 +2,50 @@ import tensorflow as tf
 import os
 import matplotlib.pyplot as plt
 
-x1 = tf.placeholder(tf.float32, name = "x1")
-x2 = tf.placeholder(tf.float32, name = "x2")
-y = tf.placeholder(tf.float32, name = "y")
+x1 = tf.placeholder(tf.float32, name="x1")
+x2 = tf.placeholder(tf.float32, name="x2")
+y = tf.placeholder(tf.float32, name="y")
 
 W = tf.Variable([0.1, 0.1], dtype=tf.float32)
 b = tf.Variable([0.1], dtype=tf.float32)
 
 x1_train = [0, 0, 1, 1]
 x2_train = [0, 1, 0, 1]
-y_train  = [0, 0, 0, 1]
+y_train = [0, 0, 0, 1]
 
-y_hat = tf.sigmoid(W[0]*x1 + W[1]*x2+b)
+y_hat = tf.sigmoid(W[0] * x1 + W[1] * x2 + b)
 loss = tf.reduce_sum(tf.squared_difference(y_hat, y))
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
-print(sess.run(loss, {x1: x1_train, x2: x2_train, y:y_train}))
+print(sess.run(loss, {x1: x1_train, x2: x2_train, y: y_train}))
 
-learn_rate = .01
+learn_rate = .5
 optimizer = tf.train.GradientDescentOptimizer(learn_rate)
 train = optimizer.minimize(loss)
 
-log_path = os.path.dirname(os.path.abspath(__file__))+"/logs"
-graph_writer = tf.summary.FileWriter(log_path , sess.graph)
+log_path = os.path.dirname(os.path.abspath(__file__)) + "/logs"
+graph_writer = tf.summary.FileWriter(log_path, sess.graph)
 
 n_epochs = 5000
 cost_vector = []
 for i in range(n_epochs):
-    _, loss_value = sess.run([train, loss], {x1: x1_train, x2: x2_train, y:y_train})
+    _, loss_value = sess.run([train, loss], {
+        x1: x1_train,
+        x2: x2_train,
+        y: y_train
+    })
     cost_vector.append(loss_value)
 
 plt.plot(range(n_epochs), cost_vector)
 plt.ylabel("cost")
 plt.show()
 
-W_opt, b_opt, loss_opt, y_hat= sess.run([W, b, loss, y_hat], {x1: x1_train, x2: x2_train, y:y_train})
-print("W = {} , b = {} , loss= {}, y_hat= {}".format(W_opt, b_opt, loss_opt, y_hat))
+W_opt, b_opt, loss_opt, y_hat = sess.run([W, b, loss, y_hat], {
+    x1: x1_train,
+    x2: x2_train,
+    y: y_train
+})
+print("W = {} , b = {} , loss= {}, y_hat= {}".format(W_opt, b_opt, loss_opt,
+                                                     y_hat))
